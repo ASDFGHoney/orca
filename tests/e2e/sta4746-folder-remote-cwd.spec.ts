22b11ba06456c14d98268e3f3ae8aff3af011755
Jinwoo-H <jinwoo0825@gmail.com>
2026-08-18 19:32:05 -0400

    test(e2e): merge duplicate node:fs import

--- tests/e2e/sta4746-folder-remote-cwd.spec.ts
+++ tests/e2e/sta4746-folder-remote-cwd.spec.ts
@@ -1,5 +1,4 @@
-import { mkdtempSync, mkdirSync } from 'node:fs'
-import { realpathSync } from 'node:fs'
+import { mkdirSync, mkdtempSync, realpathSync } from 'node:fs'
 import os from 'node:os'
 import path from 'node:path'
 
